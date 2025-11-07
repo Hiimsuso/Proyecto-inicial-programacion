b2045ed59c92bf591b78638882b50b8fd83a13a7
Hiimsuso <suso3543@gmail.com>
2025-11-07 18:23:32 +0100

    update

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    //esto es un comentario
+    // TODO me falta esto
+    public static void main(String[] args){
+    System.out.println("Este es mi primer programa en Java");
+    System.out.println("Este es la segunda línea del programa");
+
+    }
+
+    public void nombreMetodo(){
+
+    }
+
 }
